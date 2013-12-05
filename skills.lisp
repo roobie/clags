d313563e45dd48892bc6f6182f7312ac73a4a416
Björn Roberg <bjorn.roberg@gmail.com>
2013-12-05 23:53:56 +0100

    added skills.lisp that contains skill class and definitions

---
+++ skills.lisp
@@ -0,0 +1,17 @@
+(load "utils.lisp")
+
+(defclass skill ()
+  ((name :accessor name
+         :initarg :name)
+   (experience :accessor experience
+               :initarg :experience
+               :initform 0)
+   (synergies :accessor synergies
+              :initarg :synergies
+              :initform '())))
+
+(defvar *skills* (make-hash-table))
+(add-instance-maker *skills* :spotting
+                    'skill
+                    :name :spotting
+                    :synergies '(:psyche :sight))
