bc341e6e26b80a040ced981708a7f96a769ebb50
Björn Roberg <bjorn.roberg@gmail.com>
2013-12-06 00:54:17 +0100

    added new macro to instantiate class without having to quote class and same in add-instance-maker

--- utils.lisp
+++ utils.lisp
@@ -1,8 +1,13 @@
 
-(defmacro add-instance-maker (hash-table key class &rest initargs)
-  `(let ((maker (lambda () (make-instance ,class ,@initargs))))
-     (setf (gethash ,key ,hash-table) maker)))
+(defmacro instantiate (class &rest initargs)
+  ;; This macro allows you to instantiate a class created by defclass
+  ;; but opposed to make-instance, you don't need to quote the class name
+  ;; ie. (instantiate my-class :slot1 "awesome")
+  `(make-instance ',class ,@initargs))
 
+(defmacro add-instance-maker (hash-table key class &rest initargs)
+  `(let ((maker (lambda () (make-instance ',class ,@initargs))))
+     (setf (gethash ,key ,hash-table) maker)))
 
 (defun radial-distance (x1 y1 x2 y2)
   (max (abs (- x1 x2))
